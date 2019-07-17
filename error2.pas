51475c4d7bb9f165ddb3419a7682d51b8c98df9b
mayor-william <52464658+mayor-william@users.noreply.github.com>
2019-07-17 12:44:07 -0500

    Add files via upload

---
+++ error2.pas
@@ -0,0 +1,8 @@
+PROGRAM NameError2;
+VAR
+   b : INTEGER;
+   a : INTEGER;
+BEGIN
+   b := 1;
+   a := b + 2;
+END.
---
+++ testbolean.pas
@@ -0,0 +1,8 @@
+PROGRAM testbolean;
+VAR
+   b : INTEGER;
+   a : INTEGER;
+BEGIN
+   b := 1;
+   a := b + 2;
+END.
